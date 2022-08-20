382cb8dfb834f0a79f0c626a0e67d5c456ebb2e5
manuarg1231~ <manu571@gmail.com>
2022-08-20 09:23:35 -0300

    first commit

---
+++ src/ejerciciostema4_introduccionp/Persona.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejerciciostema4_introduccionp;
+
+/**
+ *
+ * @author MANUEL RIVAS GAUNA
+ */
+public class Persona {
+    
+}
